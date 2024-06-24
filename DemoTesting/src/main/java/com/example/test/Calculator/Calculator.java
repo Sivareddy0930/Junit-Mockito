f390f81c7ef21b040ab7f840bc19e6f78e6f329c
Sivareddy0930 <sivareddy0930@gmail.com>
2024-06-24 23:30:45 +0530

    junit

---
+++ DemoTesting/src/main/java/com/example/test/Calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.example.test.Calculator;
+
+public class Calculator {
+	
+	public Integer sum(Integer a,Integer b) {
+		return a+b;
+	}
+	
+	public Integer multiply(Integer a,Integer b) {
+		return a*b;
+	}
+
+}
